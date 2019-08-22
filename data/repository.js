cc331ccc8b860592c7cd7ec4b95b747bead25af7
Justyn Pisarsky <Scerelyn@gmail.com>
2019-08-22 11:11:32 -0600

    Add basic functionality to repository.js

--- data/repository.js
+++ data/repository.js
@@ -4,13 +4,13 @@ const mongoConnection = 'mongodb://localhost:27017/';
 class Repository {
     constructor(){
         this.mongo = require('mongodb').MongoClient;
-        this.mongoConnection = 'mongodb+srv://dbappuser:VerySecure24@cluster0-ouwzy.mongodb.net/test?retryWrites=true&w=majority';
+        this.mongoConnection = 'mongodb+srv://dbappuser:VerySecure123@cluster0-ouwzy.mongodb.net/test?retryWrites=true&w=majority';
         this.mongoDatabase = 'Expressive';
         this.mongoCollection = 'Users-testing'; //change to users for non testing purposes
         this.mongoOptions = { "useUnifiedTopology": "true", "useNewUrlParser": "true" };
     }
 
-    getUsers(){
+    async getUsers(query){
         let data = [];
         let client = null;
         try{
@@ -27,8 +27,8 @@ class Repository {
         return data;
     }
 
-    getAllUsers(){
-
+    async getAllUsers(){
+        return await this.getUsers({});
     }
 }
 
