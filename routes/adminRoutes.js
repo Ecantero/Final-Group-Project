711b45a23017c200c4996ea326972817af6f90a0
Justyn Pisarsky <Scerelyn@gmail.com>
2019-08-28 08:34:43 -0600

    remove unneeded printlines

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,12 +2,10 @@ let repo = require('../data/repository');
 
 exports.admin = async (req, res) => {
     let users = await repo.getAllScrubbedUsers();
-    //console.log(users);
     res.render('admin', { "users": users })
 }
 exports.suspend = async (req, res) => {
     let formData = req.body;
-    let result = await repo.updateSuspension(formData.id, formData.suspend === 'on' ? true : false);
-    console.log(result);
+    await repo.updateSuspension(formData.id, formData.suspend === 'on' ? true : false);
     res.redirect('/admin');
 }
