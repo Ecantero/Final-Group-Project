b0c3b052d713758b0631d27ae126c02e174dda4a
Ernesto Cantero <ecantero@student.neumont.edu>
2019-08-29 10:09:02 -0600

    Added require.js to use in the client side

--- public/script.js
+++ public/script.js
@@ -1,5 +1,51 @@
-var Chart = require('chart.js');
+define(function(require, exports, module) {
+  var Chart = require('chart.js');
+  var repo = require('../data/repository');
 
-const ctx = document.getElementById("Music");
+  const ctx = document.getElementById("Music");
 
-var myChart = new Chart(ctx, {});
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Favorite Genre",
+          data: [repo.getQuestionData()],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        xAxes: [
+          {
+            barPercentage: 0.5,
+            barThickness: 6,
+            maxBarThickness: 8,
+            minBarLength: 2,
+            gridLines: {
+              offsetGridLines: true
+            }
+          }
+        ]
+      }
+    }
+  });
+});
