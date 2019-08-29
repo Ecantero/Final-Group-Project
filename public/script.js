682cf7e044314b332059f2d4e0588b6a81399a4b
Ernesto Cantero <ecantero@student.neumont.edu>
2019-08-29 11:21:51 -0600

    changes to the script js file

--- public/script.js
+++ public/script.js
@@ -1,51 +1,158 @@
 define(function(require, exports, module) {
-  var Chart = require('chart.js');
-  var repo = require('../data/repository');
+  var Chart = require("chart.js");
+  var repo = require("../data/repository");
+  const Music = document.getElementById("Music");
+  const Game = document.getElementById("Game");
+  const Movie = document.getElementById("Movie");
 
-  const ctx = document.getElementById("Music");
+  const getData = () => {
+    var list = new XMLHttpRequest();
+    list.onreadystatechange = () => {
+      if (list.readyState != 4) {
+        return;
+      }
+      if (list.status >= 200 && list.status <= 300) {
+        return list.responseText;
+      }
+    };
+    list.open("get", "localhost:3000/data");
+    list.send();
+  };
 
-  var myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: [],
-      datasets: [
-        {
-          label: "Favorite Genre",
-          data: [repo.getQuestionData()],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)"
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)"
-          ],
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      scales: {
-        xAxes: [
+  const getMusicCharts = async () => {
+    const data = await repo.getQuestionData();
+    var musicChart = new Chart(Music, {
+      type: "bar",
+      data: {
+        labels: [],
+        datasets: [
           {
-            barPercentage: 0.5,
-            barThickness: 6,
-            maxBarThickness: 8,
-            minBarLength: 2,
-            gridLines: {
-              offsetGridLines: true
-            }
+            label: "Favorite Genre",
+            data: data[0],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
           }
         ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              barPercentage: 0.5,
+              barThickness: 6,
+              maxBarThickness: 8,
+              minBarLength: 2,
+              gridLines: {
+                offsetGridLines: true
+              }
+            }
+          ]
+        }
       }
-    }
-  });
+    });
+    var gameChart = new Chart(Game, {
+      type: "bar",
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Favorite Genre",
+            data: repo.getQuestionData()[0],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              barPercentage: 0.5,
+              barThickness: 6,
+              maxBarThickness: 8,
+              minBarLength: 2,
+              gridLines: {
+                offsetGridLines: true
+              }
+            }
+          ]
+        }
+      }
+    });
+
+    var movieChart = new Chart(Movie, {
+      type: "bar",
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Favorite Genre",
+            data: repo.getQuestionData(),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              barPercentage: 0.5,
+              barThickness: 6,
+              maxBarThickness: 8,
+              minBarLength: 2,
+              gridLines: {
+                offsetGridLines: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  };
 });
